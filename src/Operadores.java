c5d0f1c3398357658d140e8886fd540af0090ac4
Gustavo Miguel <gustavomiguelsilvafc@gmail.com>
2024-09-25 00:57:51 -0300

    var bool

---
+++ src/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+
+        String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
+        System.out.println(nomeCompleto);
+
+        variavel = !variavel;
+        System.out.println(variavel);
+
+
+    }
+}
---
+++ src/TiposVariaveis.java
@@ -0,0 +1,18 @@
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+       double salarioMinimo = 2500.33;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+       int num = 5;
+
+       num = 10;
+
+       System.out.println(num);
+
+       final double VALOR_DE_PI = 3.14;
+    }
+}
